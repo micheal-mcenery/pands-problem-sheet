37df7be8e2077c31f2b33b87af8a628ff1f5ddee
unknown <G00411327@gmit.ie>
2022-03-28 13:49:49 +0100

    Week 4 task: Collatz

--- collatz.py
+++ collatz.py
@@ -7,17 +7,19 @@
 # This line records the user input as "p_integer" and converts it into an integer.
 p_integer = int(input("Please enter a positive integer: "))
 
-# This line stores the output which will eventually be printed below.
+# This line stores the output which will eventually be printed below. Initailly this variable only contains the user input (converted to a string) and a space (" ").
 output = str(p_integer) + " "
 
 # This while loop will continue looping the if statement until the integer = 1.
 while p_integer != 1:
-    # The if statement assesses whether the positive integer is even or odd, performs the relevent calculations, and adds the result to the output variable in sequencial order.
+    # The if statement assesses whether the positive integer is even or odd, performs the relevent calculations, and adds the result to the output variable.
     if p_integer % 2 == 0:
         p_integer = p_integer // 2
+        # The below line updates the output varable, so that p_integer (following calculation) is added to the output varable, as well as a space (" ") (same on  line 22).
         output = output + str(p_integer) + " "
     else:
         p_integer = (p_integer * 3) + 1
         output = output + str(p_integer) + " "
 
+# This line prints the output.
 print(output)
